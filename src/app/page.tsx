9838cb9113764d93cf262a4816d04ae51205bb5c
famfrank1298 <68818256+famfrank1298@users.noreply.github.com>
2024-08-06 16:20:38 -0500

    got firebase set up and connected chat messages to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Send } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Message from "./components/Message/Message";
+import { doc, updateDoc, getDoc } from "firebase/firestore";
+import { db, dbName } from "./firebase";
 
 export default function Home() {
   const [messages, setMessages] = useState([
@@ -21,6 +23,7 @@ export default function Home() {
       ...messages,
       { role: "user", content: message },
     ]);
+
     const response = await fetch("api/chat", {
       method: "POST",
       headers: {
@@ -28,12 +31,53 @@ export default function Home() {
       },
       body: JSON.stringify([...messages, { role: "user", content: message }]),
     });
+
     const data = await response.json();
     setMessages((messages) => [
       ...messages,
       { role: "assistant", content: data.message },
     ]);
+
+    // updating user db with new messages
+    const docRef = doc(db, dbName, "test101@gmail.com");
+    await updateDoc(docRef, {
+      chat: messages,
+    });
   };
+
+  // retrieving previous message data
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const docRef = doc(db, dbName, "test101@gmail.com");
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          const prevMsg = docSnap.data().chat;
+          setMessages([
+            ...prevMsg,
+            {
+              role: "assistant",
+              content: "Hi! Welcome back. How can I help you today?",
+            },
+          ]);
+        } else {
+          setMessages([
+            {
+              role: "assistant",
+              content:
+                "Hi! I'm the FamFinance support assistant. How can I help you today?",
+            },
+          ]);
+          console.log("No such document!");
+        }
+      } catch (error) {
+        console.error("Error fetching document:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <main className="fixed h-full w-full  bg-muted">
