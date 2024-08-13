4797ce08a58edf9be4d653e72141ffbda237b2d5
famfrank1298 <68818256+famfrank1298@users.noreply.github.com>
2024-08-12 20:47:42 -0500

    changed favicon and added disclaimer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,13 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="text-center pb-4 footer-container">
-      © All Rights Reserved since 2024
+      <p className="footer-body">
+        <b>Disclaimer</b>: The information on this website is for general
+        informational purposes only and does not constitute financial,
+        investment, or legal advice. Always consult with a qualified
+        professional before making any financial decisions.
+      </p>
+      <br /> © All Rights Reserved since 2024
     </div>
   );
 }
